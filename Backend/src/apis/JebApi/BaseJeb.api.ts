fb56c1ab5d4b3b5811ffdb7fca1cade94a1bdc77
joan cordelier <joan.cordelier@epitech.eu>
2025-09-02 14:24:46 +0200

    new prisma schema and base request Jeb api

--- Backend/src/apis/JebApi/BaseJeb.api.ts
+++ Backend/src/apis/JebApi/BaseJeb.api.ts
@@ -1,2 +1,36 @@
-import { JEB_URL } from "../../global";
+import { JEB_URL, JEB_API_KEY } from "../../global";
 
+
+class BaseJebApi {
+    protected baseUrl: string;
+    protected apiKey?: string;
+
+    constructor() {
+        this.baseUrl = JEB_URL;
+        this.apiKey = JEB_API_KEY;
+    }
+
+    protected async request<T>(url: string, options: RequestInit = {}): Promise<T> {
+        const headers = {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/json',
+            ...options.headers,
+        };
+
+        try {
+            const response = await fetch(url, { ...options, headers });
+
+            if (!response.ok) {
+                const errorResponse = await response.json();
+                console.log(errorResponse);
+                console.error(`Error in request to ${url}: ${JSON.stringify(errorResponse)}`);
+                throw { message: errorResponse, status: response.status };
+            }
+
+            return response.json() as T;
+        } catch (error) {
+            console.error(`Error occurred in request to ${url}:`, error);
+            throw error;
+        }
+    }
+}
--- Backend/src/global.ts
+++ Backend/src/global.ts
@@ -1,1 +1,2 @@
 export const JEB_URL = "https://api.jeb-incubator.com";
+export const JEB_API_KEY = process.env.API_KEY;
