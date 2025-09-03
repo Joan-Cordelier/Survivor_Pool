398f9089fadcf241013142e8e8e00387e2b99672
siickzz <sachou@epitech.eu>
2025-09-03 12:02:00 +0200

    ADD: Authentication Routes & Controllers

---
+++ Backend/src/routes/Auth.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+import * as AuthController from '../controller/Auth.controller';
+
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+
+export default router;
