bb34004e754d0b69f1c2ea498a989ff35d1cd34d
sacha-vedovatti <sacha.vedovatti@epitech.eu>
2025-09-03 15:03:14 +0200

    SET: User (POST, DELETE) requests permissions.

--- Backend/src/routes/User.route.ts
+++ Backend/src/routes/User.route.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 import { authorizeRoles, requireAuth } from '../middleware/auth';
 import * as UserController from '../controller/User.controller';
 
-router.post('/create', UserController.createUserController);
+router.post('/create', requireAuth, authorizeRoles('admin'), UserController.createUserController);
 router.get('/get/:id', UserController.getUserByIdController);
 router.get('/get', UserController.getAllUsersController);
 router.delete('/delete/:id', requireAuth, authorizeRoles('admin'), UserController.deleteUserController);
