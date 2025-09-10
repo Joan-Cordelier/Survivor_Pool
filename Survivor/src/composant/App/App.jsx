19cbd74db910ccac25d1087aba4f74eb8d85527d
mike <mike.mathieu@epitech.eu>
2025-09-11 00:36:41 +0200

    ADD: about page

--- Survivor/src/composant/App/App.jsx
+++ Survivor/src/composant/App/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import GoogleAnalytics from '../BaliseGoogle/GloogleAnalytics.jsx';
 // Importation de tes composants
 import Home from '../Home/Home.jsx';
-import Rendezvous from '../Event/event.jsx';
-import Contact from '../Contact/Contact.jsx';
+import Event from '../Event/event.jsx';
+import About from '../About/About.jsx';
 import Profile from '../Profile/Profile.jsx';
 import Header from '../template/Headers/Headers.jsx';  // Ton header
-import Offre from '../Project/project.jsx';  // Ton offre
+import Project from '../Project/project.jsx';  // Ton offre
 import News from '../News/news.jsx';
 import Login from '../Login/Login.jsx'
 import Dashboard from '../Dashboard/Dashboard.jsx';
@@ -18,13 +17,12 @@ import Footer from '../template/Footer/Footer.jsx';
 function App() {
   return (
     <BrowserRouter basename="/Survivor_Pool">
-      <GoogleAnalytics />
       <Header />  {/* Le Header sera affiché sur toutes les pages */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Projects" element={<Offre />} />
-        <Route path="/Events" element={<Rendezvous />} />
-        <Route path="/About" element={<Contact />} />
+        <Route path="/Projects" element={<Project />} />
+        <Route path="/Events" element={<Event />} />
+        <Route path="/About" element={<About />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/News" element={<News />} />
         <Route path="/Login" element={<Login />} />
